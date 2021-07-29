de114d3c44097fd2394ac7273a76ff9ee14f5747
Saif Ali <saifali7646@gmail.com>
2021-07-29 00:34:36 +0000

    added event handler CTRL + C

--- play.js
+++ play.js
@@ -1,6 +1,23 @@
 const net = require("net");
 const connect = require('./client.js')
 
+const handleUserInput = function (key) {
+  // your code here 
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
